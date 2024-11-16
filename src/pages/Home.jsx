f00ee3735bcb96ace0818b1c8fc48bafce926a61
Ashique <ansariazad7864@gmail.com>
2024-11-16 21:09:38 +0530

    some new code added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
   return (
     <div>
         <HeroSection/>
-        <AboutSummary/>
+        <AboutSummary/>     
         <Skills/>
         <div>
         <Projects limit={4} />
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -16,7 +16,6 @@ const Skills = () => {
   const [isVisible, setIsVisible] = useState(false)
   const skillsRef = useRef(null)
 
-  // Technical skills data
   const skillsData = [
     {
       name: 'HTML5',
@@ -72,7 +71,6 @@ const Skills = () => {
     }
   ]
 
-  // Soft skills data for circular progress bars
   const softSkillsData = [
     {
       name: 'Adaptability',
@@ -122,36 +120,43 @@ const Skills = () => {
 
   return (
     <section className='relative w-full'>
-      <div className='w-10/12 mx-auto'>
-      <h1 className='mb-8 text-3xl font-bold text-center' data-aos="fade-left" data-aos-duration="600"  >My Skills</h1>
+      <div className='w-11/12 mx-auto sm:w-10/12'>
+        <h1
+          className='mb-8 text-2xl font-bold text-center md:text-3xl'
+          data-aos="fade-left"
+          data-aos-duration="600"
+        >
+          My Skills
+        </h1>
         <div
-          className='flex flex-col items-center w-full max-w-6xl gap-10 mx-auto text-white md:flex-row skills'
+          className='flex flex-col items-center w-full max-w-6xl gap-16 px-8 mx-auto text-white-100 md:flex-row skills md:gap-24'
           ref={skillsRef}
           data-aos='fade-up'
         >
           {/* Left side: Linear skill bars */}
           <div className='w-full md:w-1/2'>
-            
-            <ul className='space-y-3 '>
+            <ul className='space-y-6'>
               {skillsData.map((skill, index) => (
                 <li
                   key={index}
                   data-aos={skill.aos}
                   data-aos-delay={index * 100}
-                  className='gap-2 '
+                  className='gap-2'
                 >
-                  <div className='flex transition-all transform duration-600 hover:scale-105'>
-                    <div className='flex items-center justify-center w-12 h-12 text-white bg-gray-800 rounded-full'>
+                  <div className='flex p-2 transition-all transform rounded-full shadow-lg duration-600 hover:scale-105 hover:shadow-blue-500'>
+                    <div className='flex items-center justify-center w-10 h-10 bg-gray-800 rounded-full text-white-100 sm:w-12 sm:h-12'>
                       {skill.icon}
                     </div>
-                    <div className=''>
-                      <h3 className='font-sans'>{skill.name}</h3>
-                      <div className='h-2 overflow-hidden bg-gray-700 border border-gray-800 rounded shadow-lg w-[20rem] bar '>
+                    <div className='ml-3'>
+                      <h3 className='font-sans text-sm sm:text-base'>
+                        {skill.name}
+                      </h3>
+                      <div className='h-2 bg-gray-700 border border-gray-800 rounded shadow-lg w-[16rem] sm:w-[20rem]'>
                         <div
-                          className='h-full bg-blue-500 '
+                          className='h-full bg-blue-500'
                           style={{
                             width: isVisible ? `${skill.level}%` : '0%',
-                            transition: 'width 3s ease-in-out '
+                            transition: 'width 3s ease-in-out'
                           }}
                         ></div>
                       </div>
@@ -164,13 +169,11 @@ const Skills = () => {
 
           {/* Right side: Circular progress bars */}
           <div
-            className='w-full md:w-1/2 md:mt-0'
+            className='w-full md:w-1/2'
             data-aos='fade-up'
             data-aos-delay='500'
           >
-            {/* <h1 className='mb-8 text-3xl font-bold text-center'>Soft Skills</h1> */}
-
-            <div className='grid grid-cols-3 gap-6'>
+            <div className='grid grid-cols-2 gap-8 sm:grid-cols-3'>
               {softSkillsData.map((softSkill, index) => (
                 <div
                   key={index}
@@ -178,56 +181,48 @@ const Skills = () => {
                   data-aos='zoom-in-up'
                   data-aos-delay={index * 100}
                 >
-                  <div className='transition-all transform duration-600 hover:scale-105'>
-                  <div className='relative flex items-center justify-center w-24 h-24 md:w-32 md:h-32 '>
-                    {/* SVG Circular progress bar with stroke animation */}
-                    <svg
-                      width='100%'
-                      height='100%'
-                      viewBox='0 0 36 36'
-                      className='circular-progress'
-                    >
-                      <circle
-                        cx='18'
-                        cy='18'
-                        r='15.915'
-                        stroke='#2d2d2d' // Dark shade background
-                        strokeWidth='3'
-                        fill='none'
-                      />
-                      <circle
-                        cx='18'
-                        cy='18'
-                        r='15.915'
-                        stroke='#4A90E2' // Darker blue for progress
-                        strokeWidth='3'
-                        fill='none'
-                        strokeDasharray={`${
-                          isVisible
-                            ? (softSkill.level * 2 * Math.PI * 15.915) / 100
-                            : 0
-                        }, 1000`}
-                        strokeDashoffset='25.1327' // This is for 0% at the start
-                        style={{
-                          transition: 'stroke-dasharray 2.5s ease-in-out',
-                        }}
-                      />
-                    </svg>
-                    {/* Percentage text inside the circle */}
-                    {/* <span className="absolute text-lg font-semibold text-white">
-                      {isVisible ? softSkill.level : 0}%
-                    </span> */}
-                    {/* Icon in the center of the circle */}
-                    <div className='absolute flex items-center justify-center w-12 h-12 bg-opacity-50 rounded-full bg-black-900 text-white-100'>
-                      {softSkill.icon}
+                  <div className='p-4 transition-all transform rounded-lg shadow-lg duration-600 hover:scale-105 hover:shadow-blue-500'>
+                    <div className='relative flex items-center justify-center w-20 h-20 md:w-24 md:h-24'>
+                      <svg
+                        width='100%'
+                        height='100%'
+                        viewBox='0 0 36 36'
+                        className='circular-progress'
+                      >
+                        <circle
+                          cx='18'
+                          cy='18'
+                          r='15.915'
+                          stroke='#2d2d2d'
+                          strokeWidth='3'
+                          fill='none'
+                        />
+                        <circle
+                          cx='18'
+                          cy='18'
+                          r='15.915'
+                          stroke='#4A90E2'
+                          strokeWidth='3'
+                          fill='none'
+                          strokeDasharray={`${
+                            isVisible
+                              ? (softSkill.level * 2 * Math.PI * 15.915) / 100
+                              : 0
+                          }, 1000`}
+                          strokeDashoffset='25.1327'
+                          style={{
+                            transition: 'stroke-dasharray 2.5s ease-in-out'
+                          }}
+                        />
+                      </svg>
+                      <div className='absolute flex items-center justify-center w-10 h-10 bg-opacity-50 rounded-full bg-black-900 text-white-100'>
+                        {softSkill.icon}
+                      </div>
                     </div>
-                   
-                  </div> 
-                  <p className='mt-2 font-sans text-center text-white-100'>
+                    <p className='mt-2 font-sans text-sm text-center text-white-100'>
                       {softSkill.name}
                     </p>
                   </div>
-                  
                 </div>
               ))}
             </div>
