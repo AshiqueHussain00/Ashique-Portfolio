6af5d4615dedc9c8c01fefb657f37e9c6f66aed2
Ashique <ansariazad7864@gmail.com>
2024-11-22 20:01:48 +0530

    ..

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Projects from './Projects'
 import Certificate from './Certificate'
-import HeroSection from './Herosection'
+import HeroSection from './HeroSection'
 import AboutSummary from './AboutSummary'
 import Skills from './Skills'
 
